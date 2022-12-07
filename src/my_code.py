b335c8f7461877f3d169b80896c6456c04923e66
owen <26ohughes@athenian.org>
2022-12-07 12:35:58 -0800

    done

--- src/my_code.py
+++ src/my_code.py
@@ -1,7 +1,7 @@
 #############################
 # Collaborators & Sources: (enter people or resources who/that helped you)
-# 
-#
+# my family played and tested the game along with Brennan, Alex, Penelope
+# https://www.datacamp.com/tutorial/python-arrays helped me with arrays
 #
 #############################
 import random
@@ -17,14 +17,14 @@ def lose():
 game_playing = input("would you like to play pigeon simulator ")
 money = 0
 inventory = numpy.array([0, 0, 0])
-while game_playing == "pigeon simulator" or "yes":
+while game_playing == "pigeon simulator" or "yes":  # repeats the things below until you win or quit
     fate = input("do you want to beg for crumbs, sell things, buy things, steal bread, steal a pizza or end game ")
     chance = random.randint(1, 100)
     beg_money = random.randint(1, 10)
     bank_money = random.randint(1000, 1500)
     steal_money = random.randint(10, 30)
     donuts = int(inventory[0])
-    if fate == "beg for crumbs" or fate == "beg":
+    if fate == "beg for crumbs" or fate == "beg":  # gives the user either 1-10 crumbs or nothing
         if 50 < chance <= 100:
             print(f"you earned {beg_money} crumbs")
             money += beg_money
@@ -32,11 +32,11 @@ while game_playing == "pigeon simulator" or "yes":
         elif 1 <= chance < 50:
             print("you didn't get any crumbs")
             print(f"you have {money} crumbs")
-    if fate == "buy things" or fate == "buy":  # this is the shop
-        shop = input("would you like to buy a baguette(might sell for a higher value) for 5 dollars, "
+    if fate == "buy things" or fate == "buy":  # this is the shop where you can buy upgrades
+        shop = input("would you like to buy a baguette(might sell for a higher value) for 5 crumbs, "
                      "a human costume(increases\n "
-                     "the chances to steal a pizza) for 300 dollars, beak extension(increases the chance of stealing \n"
-                     "bread) for 100 or a bakery(win the game) for 3,000 crumbs ")
+                     "the chances to steal a pizza) for 300 crumbs, beak extension(increases the chance of stealing \n"
+                     "bread) for 100 crumbs or a bakery(win the game) for 3,000 crumbs ")
         if shop == "baguette":
             if money >= 5:
                 inventory[0] += 1
@@ -45,7 +45,7 @@ while game_playing == "pigeon simulator" or "yes":
                 print(f"you have {money} crumbs")
             else:
                 print("you don't have enough crumbs")
-        if shop == "human costume":
+        elif shop == "human costume":
             if money >= 300:
                 inventory[1] += 1
                 print("you bought a human costume")
@@ -53,46 +53,66 @@ while game_playing == "pigeon simulator" or "yes":
                 print(f"you have {money} crumbs")
             else:
                 print("you don't have enough crumbs")
-        if shop == "beak extension":
+        elif shop == "beak extension":
             if money >= 100:
                 inventory[2] += 1
                 print("you bought a beak extension")
                 money -= 100
                 print(f"you have {money} crumbs")
             else:
-                print("you don't have enough crumbs")  # end of shop
-        if shop == "bakery" or "a bakery":
+                print("you don't have enough crumbs")
+        elif shop == "bakery" or "a bakery":
             if money >= 3000:
                 for i in range(3):
                     if i == 0:
                         print("you won, you now have an easy way to get bread!")
                     elif i == 1:
                         print("your so cool!")
-                    else:
+                    elif i == 2:
                         print("your such a gamer!")
                         break
             else:
-                print("you don't have enough money")
-    if fate == "steal a pizza":
-        if inventory[1] > 0:
-            if chance >= 50:
-                print(f"you successfully stole a pizza and earned {bank_money} crumbs")
-                money += bank_money
-                print(f"you have {money} crumbs")
+                print("you don't have enough crumbs")
+    if fate == "steal a pizza":  # high chance to lose all crumbs high reward upgrade increases chances
+        if money > 0:
+            if inventory[1] > 0:
+                if chance >= 50:
+                    print(f"you successfully stole a pizza and earned {bank_money} crumbs")
+                    money += bank_money
+                    print(f"you have {money} crumbs")
+                else:
+                    print("you got caught and lost all your crumbs")
+                    money -= money
+                    lose()
             else:
-                print("you got caught and lost all your crumbs")
-                money -= money
-                lose()
+                if chance >= 95:
+                    print(f"you successfully stole a pizza and earned {bank_money} crumbs")
+                    money += bank_money
+                    print(f"you have {money} crumbs")
+                else:
+                    print("you got caught and lost all your crumbs")
+                    money -= money
+                    lose()
         else:
-            if chance >= 95:
-                print(f"you successfully stole a pizza and earned {bank_money} crumbs")
-                money += bank_money
-                print(f"you have {money} crumbs")
+            if inventory[1] > 0:
+                if chance >= 50:
+                    print(f"you successfully stole a pizza and earned {bank_money} crumbs")
+                    money += bank_money
+                    print(f"you have {money} crumbs")
+                else:
+                    print("you got caught and lost all your crumbs")
+                    money -= money
+                    lose()
             else:
-                print("you got caught and lost all your crumbs")
-                money -= money
-                lose()
-    if fate == "steal bread" or fate == "bread":
+                if chance >= 95:
+                    print(f"you successfully stole a pizza and earned {bank_money} crumbs")
+                    money += bank_money
+                    print(f"you have {money} crumbs")
+                else:
+                    print("you got caught and got fined for your crime")
+                    money -= 200
+                    print(f"you have {money} crumbs")
+    if fate == "steal bread" or fate == "bread":  # low chance to lose all crumbs, high chance to get crumbs/N/A
         if inventory[2] == 0:
             if 1 <= chance <= 10:
                 print("you got caught and lost all your crumbs")
@@ -115,7 +135,7 @@ while game_playing == "pigeon simulator" or "yes":
                 print(f"you earned {steal_money} crumbs")
                 money += steal_money
                 print(f"you have {money} crumbs")
-    if fate == "sell" or fate == "sell things":
+    if fate == "sell" or fate == "sell things":  # lets you sell baguettes
         answer = input("would you like to sell baguettes ")
         if answer == "baguettes" or answer == "baguette" or answer == "yes":
             if inventory[0] > 0:
